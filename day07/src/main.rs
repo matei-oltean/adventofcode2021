8bf674e18f021b3c7dfda6043e85b32f453ceb51
Matei Oltean <matei.oltean@gmail.com>
2021-12-07 11:35:36 +0100

    Add day07

---
+++ day07/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+fn min_fuel(
+    depths: &Vec<isize>,
+    targets: impl Iterator<Item = isize>,
+    dist: fn(isize, isize) -> isize,
+) -> isize {
+    targets
+        .map(|m| depths.iter().fold(0, |s, &crab| s + dist(m, crab)))
+        .min()
+        .unwrap()
+}
+
+fn main() {
+    let mut depths: Vec<isize> = fs::read_to_string("./input/07")
+        .unwrap()
+        .trim()
+        .split(',')
+        .map(|s| s.parse().unwrap())
+        .collect();
+    depths.sort();
+    println!(
+        "{}",
+        min_fuel(
+            &depths,
+            [depths[depths.len() / 2 - 1], depths[depths.len() / 2]].into_iter(),
+            |x, y| (x - y).abs()
+        )
+    );
+    println!(
+        "{}",
+        min_fuel(&depths, depths[0]..=depths[depths.len() - 1], |x, y| {
+            let dist = (x - y).abs();
+            dist * (dist + 1) / 2
+        })
+    );
+}
